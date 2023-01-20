37626932d8b0a2eaf95f3f94e804b6d79f634e64
swanfly <shixianmingjava@163.com>
2023-01-20 17:52:20 +0800

    init

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "BCarousel",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
